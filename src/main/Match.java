5abdca6675bd766b915931d9436bf395c84425f7
FellipeCavalcante <azevedoaranha1@gmail.com>
2024-12-06 19:24:09 -0300

    Match: boolean sem uso removido

--- src/main/Match.java
+++ src/main/Match.java
@@ -10,7 +10,7 @@ public class Match {
      * seguido da criação de um GridBagLayout e definindo suas dimensões, fazendo a declaração de uma nova instância
      * da classe Board (Tabuleiro) e adicionando ao JFrame, definindo também a visibilidade do frame como verdadeira.
      */
-    private boolean isWhiteTurn = true;
+
     public void start() {
         JFrame frame = new JFrame();
         frame.getContentPane().setBackground(Color.BLACK);
